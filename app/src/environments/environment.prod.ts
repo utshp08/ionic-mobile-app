f6e6316e4fc7b5109706b753fa937e7d54596cba
Reymart H. Pantonia <reymart.pantonia@PERSONIV.COM>
2019-04-30 03:54:03 +0800

    fix merge conflict

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,9 +1,4 @@
 export const environment = {
-<<<<<<< HEAD
-  server_address: "https://app-ionic-server.herokuapp.com",
-  production: true
-=======
   production: true,
   server_address:  "https://app-ionic-server.herokuapp.com"
->>>>>>> b2b7bafb9926e67d65efa33c8bbe60489096b341
 };
